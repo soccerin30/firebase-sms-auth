6775e86394ed7577551ef6502047a7e5a3fad18b
soccerin30 <footballer.rin@gmail.com>
2020-11-02 22:49:00 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="tel" v-model="phoneNumber" placeholder="電話番号" />
+    <p>{{ fixedPhoneNumber }}</p>
+    <div id="recaptcha-container" />
+    <button @click="sendMessage()">送信</button>
+    <input type="number" v-model="verificationCode" placeholder="確認コード" />
+    <button @click="confirmMessage()">送信</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firebase from "firebase";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      phoneNumber: null,
+      recaptchaVerifier: null,
+      recaptchaWidgetId: null,
+      confirmationResult: null,
+      verificationCode: null,
+    };
+  },
+  computed: {
+    fixedPhoneNumber() {
+      if (this.phoneNumber) {
+        return "+81" + this.phoneNumber.slice(1);
+      }
+      return "";
+    },
+  },
+  methods: {
+    async sendMessage() {
+      try {
+        this.confirmationResult = await firebase
+          .auth()
+          .signInWithPhoneNumber(this.fixedPhoneNumber, this.recaptchaVerifier);
+        console.log(this.confirmationResult);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async confirmMessage() {
+      if (this.confirmationResult && this.verificationCode) {
+        try {
+          const result = await this.confirmationResult.confirm(
+            this.verificationCode
+          );
+          const user = result.user;
+          console.log(user);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
+  },
+  async mounted() {
+    this.recaptchaVerifier = window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "normal",
+        callback: (responseToken) => {
+          console.log(responseToken);
+        },
+      }
+    );
+    this.recaptchaWidgetId = await this.recaptchaVerifier.render();
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyCkEV52hzLPXNTV773tJeeKkFhZDo0BfLQ",
+  projectId: "sms-estra",
+  authDomain: "sms-estra.firebaseapp.com",
+  databaseURL: "https://sms-estra.firebaseio.com",
+  storageBucket: "sms-estra.appspot.com"
+}
+
+firebase.initializeApp(config)
+
+export default firebase
 
 new Vue({
   render: h => h(App),
